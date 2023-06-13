d2ce10da9eb2ad3c75473536d0cb29b4b0f7444d
Jack <omnomnomny@gmail.com>
2023-06-12 22:03:10 -0400

    new changes for testing api

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // fragments microservice API, defaults to localhost:8080
-const apiUrl = process.env.API_URL || 'http://localhost:8080';
+const apiUrl = process.env.API_URL || "http://localhost:8080";
 
 /**
  * Given an authenticated user, request all fragments for this user from the
@@ -7,18 +7,82 @@ const apiUrl = process.env.API_URL || 'http://localhost:8080';
  * to have an `idToken` attached, so we can send that along with the request.
  */
 export async function getUserFragments(user) {
-    console.log('Requesting user fragments data...');
-    try {
-        const res = await fetch(`${apiUrl}/v1/fragments`, {
-            // Generate headers with the proper Authorization bearer token to pass
-            headers: user.authorizationHeaders(),
-        });
-        if (!res.ok) {
-            throw new Error(`${res.status} ${res.statusText}`);
-        }
-        const data = await res.json();
-        console.log('Got user fragments data', { data });
-    } catch (err) {
-        console.error('Unable to call GET /v1/fragment', { err });
+  console.log("Requesting user fragments data...");
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      // Generate headers with the proper Authorization bearer token to pass
+      headers: user.authorizationHeaders(),
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
     }
+    const data = await res.json();
+    console.log("Got user fragments data", data.fragments);
+
+    // display it for the user to see
+    const listContainer = document.createElement("ul");
+    data.fragments.forEach((fragmentID) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = fragmentID;
+      listContainer.appendChild(listItem);
+    });
+    const fraglist = document.getElementById("fragmentList");
+    fraglist.innerHTML = "";
+    fraglist.appendChild(listContainer);
+  } catch (err) {
+    console.error("Unable to call GET /v1/fragment", { err });
+  }
+}
+
+export async function postUserFragments(user, data, type) {
+  console.log("Posting user fragments data...");
+  try {
+    if (type == "application/json") {
+      data = JSON.parse(JSON.stringify(data));
+    }
+
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      method: "post",
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+        "Content-type": type,
+      },
+      body: data,
+    });
+
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+
+    console.log("Posted user fragments data: ", data);
+    console.log(res);
+  } catch (err) {
+    console.error("Unable to call POST /v1/fragments", { err });
+  }
+}
+
+export async function getFragmentDataByID(user, id) {
+  try {
+    if (id != "") {
+      console.log(`Requesting fragment data by ID: ${id}`);
+      console.log(`Fetching from ${apiUrl}/v1/fragments/${id}`);
+      const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+        headers: user.authorizationHeaders(),
+      });
+
+      if (!res.ok) {
+        throw new Error(`${res.status} ${res.statusText}`);
+      }
+
+      const data = await res.text();
+      console.log("Received:", data);
+      document.getElementById("returnedData").innerHTML = data;
+    } else {
+      document.getElementById("returnedData").textContent =
+        "Error: ID required";
+      console.log("Error: ID required");
+    }
+  } catch (err) {
+    console.log(`Unable to call GET /v1/fragments/${id}`, { err });
+  }
 }
--- src/app.js
+++ src/app.js
@@ -1,47 +1,76 @@
-import { Auth, getUser } from './auth';
-import { getUserFragments } from './api';
+import { Auth, getUser } from "./auth";
+import {
+  getUserFragments,
+  getUserFragmentList,
+  postUserFragments,
+  getFragmentDataByID,
+  getFragmentInfoByID,
+  deleteFragmentDataByID,
+  updateFragmentByID,
+} from "./api";
 
 async function init() {
-    // Get our UI elements
-    const userSection = document.querySelector('#user');
-    const loginBtn = document.querySelector('#login');
-    const logoutBtn = document.querySelector('#logout');
+  // Get our UI elements
+  const userSection = document.querySelector("#user");
+  const loginButton = document.querySelector("#login");
+  const logoutButton = document.querySelector("#logout");
+  const postSection = document.querySelector("#post");
+  const postButton = document.querySelector("#postButton");
+  const getButton = document.querySelector("#getButton");
+  const getByIdButton = document.querySelector("#getByIdButton");
 
-    // Wire up event handlers to deal with login and logout.
-    loginBtn.onclick = () => {
-        // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
-        // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
-        Auth.federatedSignIn();
-    };
-    logoutBtn.onclick = () => {
-        // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
-        // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
-        Auth.signOut();
-    };
+  // Wire up event handlers to deal with login and logout.
+  loginButton.onclick = () => {
+    // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
+    // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
+    Auth.federatedSignIn();
+  };
+  logoutButton.onclick = () => {
+    // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
+    // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
+    Auth.signOut();
+  };
 
-    // See if we're signed in (i.e., we'll have a `user` object)
-    const user = await getUser();
-    if (!user) {
-        // Disable the Logout button
-        logoutBtn.disabled = true;
-        return;
-    }
+  // See if we're signed in (i.e., we'll have a `user` object)
+  const user = await getUser();
+  if (!user) {
+    // Disable the Logout button
+    logoutButton.disabled = true;
+    return;
+  }
 
-    // Do an authenticated request to the fragments API server and log the result
+  // post the fragment
+  postButton.onclick = () => {
+    let data = document.querySelector("#data").value;
+    let type = document.querySelector("#types").value;
+    postUserFragments(user, data, type);
+  };
+
+  // get the list of fragments id for the authenticated user
+  getButton.onclick = () => {
     getUserFragments(user);
+  };
 
-    // Log the user info for debugging purposes
-    console.log({ user });
+  getByIdButton.onclick = () => {
+    let id = document.querySelector("#id").value;
+    getFragmentDataByID(user, id);
+  };
 
-    // Update the UI to welcome the user
-    userSection.hidden = false;
+  // Log the user info for debugging purposes
+  console.log({ user });
 
-    // Show the user's username
-    userSection.querySelector('.username').innerText = user.username;
+  // Update the UI to welcome the user
+  userSection.hidden = false;
 
-    // Disable the Login button
-    loginBtn.disabled = true;
+  // Show the user's username
+  userSection.querySelector(".username").innerText = user.username;
+
+  // Disable the Login button
+  loginButton.disabled = true;
+  if ((loginButton.disabled = true)) {
+    postSection.hidden = false;
+  }
 }
 
 // Wait for the DOM to be ready, then start the app
-addEventListener('DOMContentLoaded', init);
+addEventListener("DOMContentLoaded", init);
